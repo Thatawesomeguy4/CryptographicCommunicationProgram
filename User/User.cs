0e0d37247fd57f44746077bf609e68b1c6a85a1f
Thatawesomeguy4 <derekteepee@gmail.com>
2021-10-13 23:14:32 -0400

    Created Basic User Class

---
+++ User/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+
+/**
+ * This class will hold the main functionality for users.
+ * 
+ * 
+ */
+namespace CryptographicCommunicationProgram.User
+{
+    class User
+    {
+        string username;
+        string password;                //this should never be in plain text. would be able to be caught by debugging programs
+        Array contacts;                 //this will be the list of other partners that the user has talked to before/registered on the platform
+        IPAddress publicIpAddress;      //This may be moved elsewhere later, for now it holds the user's public IP for setting up connections
+
+    }
+}
