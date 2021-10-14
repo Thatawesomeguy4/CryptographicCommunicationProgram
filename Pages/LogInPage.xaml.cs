d67a5b0f86f565bd7bcebc57c57e4a55a0a8e553
Thatawesomeguy4 <derekteepee@gmail.com>
2021-10-13 22:53:23 -0400

    Added First Page. Will serve to let the user log in to a profile with a username and password.

---
+++ Pages/LogInPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CryptographicCommunicationProgram.Pages
+{
+    /// <summary>
+    /// Interaction logic for LogInPage.xaml
+    /// </summary>
+    public partial class LogInPage : Page
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
